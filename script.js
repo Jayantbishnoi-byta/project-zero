9c6ed862624ea2e2fb1838fb878f419a91bd0f8b
Jayantbishnoi-byta <jayantbishnoi79@gmail.com>
2026-07-15 11:01:43 +0530

    Add saveData function to store data in localStorage

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+function saveData(){
+
+const data={
+date:document.getElementById("date").value,
+attendance:document.getElementById("attendance").value,
+research:document.getElementById("research").value,
+win:document.getElementById("win").value
+};
+
+localStorage.setItem("projectZeroToday",JSON.stringify(data));
+
+document.getElementById("status").innerHTML="✅ Saved Successfully Co-Founder!";
+}
