92d86c204747930d702f9835fdb842d8824b6c9a
Jayantbishnoi-byta <jayantbishnoi79@gmail.com>
2026-07-17 16:56:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 function saveData(){
 
-const data={
-date:document.getElementById("date").value,
-attendance:document.getElementById("attendance").value,
-research:document.getElementById("research").value,
-win:document.getElementById("win").value
+const data = {
+    date: document.getElementById("date").value,
+    attendance: document.getElementById("attendance").value,
+    research: document.getElementById("research").value,
+    win: document.getElementById("win").value,
+
+    goal: document.getElementById("goal").value,
+    goalProgress: document.getElementById("goalProgress").value
 };
-goal: document.getElementById("goal").value,
-goalProgress: document.getElementById("goalProgress").value
-let history=JSON.parse(localStorage.getItem("projectZeroHistory")) || [];
+
+let history = JSON.parse(localStorage.getItem("projectZeroHistory")) || [];
 
 history.push(data);
 
-localStorage.setItem("projectZeroHistory",JSON.stringify(history));
+localStorage.setItem("projectZeroHistory", JSON.stringify(history));
 
-document.getElementById("status").innerHTML="✅ Saved Successfully Co-Founder!";
+document.getElementById("status").innerHTML = "✅ Saved Successfully Co-Founder!";
 
 }
